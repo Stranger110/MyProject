7bafeacf5a43509376661a812aedb59cceb2fa74
Stranger110 <siddharthprajapati59@gmail.com>
2024-05-27 11:27:35 +0530

    20% work done

--- script.js
+++ script.js
@@ -14,3 +14,28 @@ let password = document.getElementById("password");
         password.type = "password";
     } 
     }
+
+    // Store into cookies
+if ($('#remember').attr('checked')) {
+    var email = $('#email').attr("value");
+    var passwrd = $('#password').attr("value");
+    // set cookies to expire in 14 days
+    $.cookie('email', email, { expires: 14 });
+    $.cookie('password', password, { expires: 14 });
+    $.cookie('remember', true, { expires: 14 });
+  } else {
+    // reset cookies
+    $.cookie('email', null);
+    $.cookie('password', null);
+    $.cookie('remember', null);
+  }
+  
+  // Read from cookies
+  var remember = $.cookie('remember');
+  if (remember == 'true') {
+    var email = $.cookie('email');
+    var passwrd = $.cookie('password');
+    // autofill the fields
+    $('#email').attr("value", email);
+    $('#password').attr("value", passwrd);
+  }
